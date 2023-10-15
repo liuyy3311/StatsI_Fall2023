bc50a28a6760bb46daf7fe1296ecec6e25e984be
liuyy3311 <144786856+liuyy3311@users.noreply.github.com>
2023-10-15 21:47:33 +0100

    Add files via upload

--- problemSets/PS02/template/PS02_Yuanyuan_Liu.R
+++ problemSets/PS02/template/PS02_Yuanyuan_Liu.R
@@ -1,3 +1,4 @@
+#Question1
 #a
 # Define the observed data
 observed <- matrix(c(14, 6, 7, 7, 7, 1), nrow = 2, ncol = 3,byrow = TRUE)
@@ -28,16 +29,15 @@ if(p_value<0.1){
   cat("under the significance level a=0.1,we fail to reject null hypothsis,the officer were less likely to solicit a bribe from drivers depending on their class.\n")
 }
 
-
 #c
-residuals <- matrix(0, nrow = 2, ncol = 3)
-for (i in 1:2) {
-  for (j in 1:3) {
-    residuals[i, j] <- (observed[i, j] - expected[i, j]) / sqrt(expected[i, j]* (1 - row_sums[i]/total_sum) * (1 - col_sums[j]/total_sum) )
-  }
-}
-residuals
-
+# Calculate the standardized residuals for each cell
+residuals <- (observed - expected) / sqrt(expected)*(1 - (row_sums/total_sum)) * (1 - (col_sums/total_sum))
+# Create an empty result table
+result_table <- matrix(NA, nrow = 2, ncol = 3)
+colnames(result_table) <- c("Not Stopped", "Bribe requested", "Stopped/given warning")
+rownames(result_table) <- c("Upper class", "Lower class")
+result_table <- format(residuals, digits = 4)
+result_table
 
 #d
 #For data with positive standardized residuals, which indicates that the observed frequency is higher than the predetermined frequency, positive residual standard deviations indicate that police are more likely to obtain tickets from these drivers. For data with negative standardized residuals, this means that the observed frequency is lower than the predetermined frequency, in which case a negative residual means that the police are less likely to obtain tickets from these drivers. Based on the above data, the social class of drivers may have an impact on bribery behavior between police and drivers. Police are more likely to demand bribes from drivers of higher social class.
@@ -55,7 +55,3 @@ X <- data$reserved
 Y <- data$water
 model <- lm(Y~X)
 summary(model)
-
-#c
-#On the confidence interval of 0.05, we can reject H0, the reservation policy has a significant effect on the number of new or repaired drinking water facilities in the villages.
-#with the increase per unit of reservation, the number of new water or repaired drinking water facilities in the villages will increase by 9.252
